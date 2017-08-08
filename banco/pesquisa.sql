b95d770540892eb754fc082673218bfd755d31a8
Wes <wesleysilva.ti@gmail.com>
2017-08-08 13:51:32 +0000

    atualizacao codeigniter

---
+++ banco/pesquisa.sql
@@ -0,0 +1,11 @@
+-- -----------------------------------------------------
+-- Select 
+-- -----------------------------------------------------
+
+SELECT * FROM westoque.usuario;
+
+SELECT * FROM westoque.fornecedor;
+
+SELECT * FROM westoque.categoria;
+
+SELECT * FROM westoque.produto;
